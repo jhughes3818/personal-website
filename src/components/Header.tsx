28d0fac57abb50267027230c003b2564414748f5
Jason <jhughes3818@gmail.com>
2023-01-05 09:23:26 +0800

    init

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+export default function Header() {
+  return (
+    <div className="grid place-items-center py-10">
+      <div className="flex py-10">
+        <div>
+          <h1 className="flex text-2xl mr-4 font-bold">👋</h1>
+        </div>
+        <div>
+          <h1 className="flex text-2xl font-bold">Hey, I'm Jason.</h1>
+          <p>For work, I'm a product manager.</p>
+          <p>I also like running and climbing mountains.</p>
+          <p>I'm a big fan of sleeping outdoors.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
