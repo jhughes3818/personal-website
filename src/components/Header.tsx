87f2bc8130f6a498b0b300c7a8f176a95a673368
Jason <jhughes3818@gmail.com>
2023-01-10 12:35:17 +0800

    added webp

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -28,15 +28,15 @@ export default function Header() {
           </a>
         </div>
         <div>
-          {/* <img
+          <img
             className="h-96 w-96 rounded-lg object-cover shadow-lg mt-5 md:mt-0"
-            src="/images/hero3purple.png"
-          ></img> */}
-          <picture>
-            <source type="image/webp" srcSet="hero3purple.webp" />
-            <source type="image/jpeg" srcSet="hero3purple.png" />
-            <img src="hero3purple.png" alt="" />
-          </picture>
+            src="/images/hero3purple.webp"
+          ></img>
+          {/* <picture>
+            <source type="image/webp" srcSet="/images/hero3purple.webp" />
+            <source type="image/jpeg" srcSet="/images/hero3purple.png" />
+            <img src="/images/hero3purple.png" alt="" />
+          </picture> */}
         </div>
       </div>
     </div>
