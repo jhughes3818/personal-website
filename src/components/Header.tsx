f2122ca877bdbfbeeb7d8d6fbec97815bb463037
Jason <jhughes3818@gmail.com>
2023-01-14 13:14:55 +0800

    add webp images

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,12 +3,11 @@ export default function Header() {
     <div className="lg:w-2/3 mx-auto p-6 mt-12 bg-purple-100 rounded-3xl">
       <div className="flex flex-col md:flex-row justify-center content-center px-4 gap-8 relative">
         <div className="grid place-items-center">
-          <img src="/images/intro-lg.png" className=""></img>
+          <img src="/images/intro-lg.webp" className=""></img>
         </div>
         <div>
-          <img className="" src="/images/headerimagelg.png"></img>
+          <img className="" src="/images/headerimagelg.webp"></img>
         </div>
-        {/* <img src="/images/group.png"></img> */}
       </div>
     </div>
   );
