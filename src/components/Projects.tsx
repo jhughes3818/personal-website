22ce218acaa9b1874b2a746bc005f764822b8a2d
Jason <jhughes3818@gmail.com>
2023-05-07 09:32:24 +1000

    add podlist

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,4 +1,5 @@
 import FantasyGrid from "./Projects/FantasyGrid";
+import PodList from "./Projects/PodList";
 
 export default function Projects() {
   return (
@@ -6,6 +7,9 @@ export default function Projects() {
       {/* <div className="lg:w-2/3 mx-auto pt-7 pb-4 px-7">
         <h1 className="text-4xl font-bold">My Projects</h1>
       </div> */}
+      <div className="lg:w-2/3 mx-auto p-7 bg-blue-100 rounded-3xl mb-4 mt-12">
+        <PodList />
+      </div>
       <div className="lg:w-2/3 mx-auto p-7 bg-blue-100 rounded-3xl mb-4 mt-12">
         <FantasyGrid />
       </div>
--- src/components/Projects/FantasyGrid.tsx
+++ src/components/Projects/FantasyGrid.tsx
@@ -1,31 +1,5 @@
 export default function FantasyGrid() {
   return (
-    // <div className="mt-3 md:flex">
-    //   <div className="max-w-sm mr-6">
-    //     <h2 className="text-3xl font-semibold text-blue-500">Fantasy Grid</h2>
-    //     <p className="text-lg text-gray-500">
-    //       I built and maintain an app for playing Formula 1 Fantasy. I was
-    //       frustrated with the official app, so I built my own. It's a full-stack
-    //       app built with React, Next.js, and Tailwind CSS. It's hosted on Vercel
-    //       and uses Supabase for the database. <br />
-    //       <br />
-    //       The primary difference from the official app is the scoring and
-    //       pricing system for the drivers, which I designed based on frustrations
-    //       myself and my friends had with the official app.
-    //     </p>
-    //     <button className="bg-blue-500 h-12 w-36 rounded-lg text-white text-lg align-bottom mt-3">
-    //       <a href="https://fantasygrid.app/">Play F1 Fantasy</a>
-    //     </button>
-    //   </div>
-    //   <div>
-    //     <img
-    //       src="/images/f1-fantasy.webp"
-    //       alt="Fantasy Grid"
-    //       className="w-96 h-96 rounded-lg object-cover shadow-lg mt-5 md:mt-0"
-    //     />
-    //   </div>
-    // </div>
-
     <div className="flex flex-col md:flex-row  content-center px-4 gap-8 relative">
       <div className="max-w-xl">
         <h2 className="text-3xl font-semibold text-blue-500">Fantasy Grid</h2>
